043367cebed8e4c43fe6e43bb832526971d571ee
Banh Duc Hieu <hieu.banh@fullertonhealth.com>
2023-05-30 16:01:35 +0700

    add api hieuDT

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3102
 
+app.get('/hieuDT', (req,res)=>{
+	res.send('My name\'s Hieu')
+})
+
 app.get('/',(req,res)=>{
 	res.send('welcome')
 })
